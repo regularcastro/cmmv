cc9a3ac14e1fb40fcd38b69fa295ab7f1b7a4f0e
irregularcastro <castro.ethng@gmail.com>
2024-01-17 15:40:02 -0300

    mais álgebra linear

--- CMMV.py
+++ CMMV.py
@@ -1,8 +1,8 @@
 import numpy as np
-p1=np.array([float(1.232),float(1.232),float(1.232)])
-p2=np.array([float(1),float(1),float(1)])
-p3=np.array([float(1.232),float(1.232)])
-p4=np.array([float(1),float(1)])
+p1=np.array([float(1.2),float(1.2),float(1.2)])
+p2=np.array([float(-1.2),float(1.3),float(1.3)])
+p3=np.array([float(1.232),float(1.232),float(1.232)])
+p4=np.array([float(1),float(-1),float(2)])
 p5=np.array([float(1.232)])
 p6=np.array([float(1),float(1)])
 p7=np.array([float(2),float(2)])
@@ -21,12 +21,8 @@ def distance(p1,p2):
 #distance(p1,p6)
 
 def vector(p1,p2):
-    e=0
-    res=[]
-    for p1[0] in p1: 
-        r=(p2[e]-p1[e])
-        res.append(r)
-    return(np.array(res))
+    r=(p2-p1)
+    return(r)
 
 def vector_modulus(v1):
     e=0
@@ -37,27 +33,17 @@ def vector_modulus(v1):
     r=sum(res[0:])**(2**-1)
     return(r)
 
-#vector_modulus(p7)
-
 def unit_vector():
     x=1
 
 ###############
 def vector_product(v1,v2,ang):
     #print(v1,v2,ang)
-    t=((3)**2**-1)
-    print(2,t,ang)
-    print((ang*np.pi)/180)
-    #print(ang*(np.pi/180))
-    print(2*t*ang)
-    
-    #return(np.array(r))
+    r=v1*v2*np.sin(ang*np.pi/180)
+    print(r)
+    return(r) 
 
-"""def vector_product(v1,v2):
-    r=vector_modulus(v1)*vector_modulus(v2)
-    print(np.array(r))
-    return(np.array(r))"""
-
-vector_product(vector_modulus(vector(p6,p7)),vector_modulus(vector(p8,p9)),30)
+print(np.sin(np.pi))
+#vector_product(vector_modulus(vector(p1 ,p2)),vector_modulus(vector(p1,p2)),30)
 
 #print(np.sin(90*np.pi*1/180))
